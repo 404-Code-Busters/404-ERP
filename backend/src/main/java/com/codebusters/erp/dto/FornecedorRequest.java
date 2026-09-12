18dd19a77da27a17fb53a717e35ba2c13aac8a07
Marcus Vinicius <marcusvini751663@gmail.com>
2026-09-12 01:41:58 -0300

    feat: complete fornecedor CRUD setup

---
+++ backend/src/main/java/com/codebusters/erp/dto/FornecedorRequest.java
@@ -0,0 +1,77 @@
+package com.codebusters.erp.dto;
+
+public class FornecedorRequest {
+
+    private String tipo;
+    private String documento;
+    private String razaoSocial;
+    private String nomeFantasia;
+    private String email;
+    private String telefone;
+    private String dadosBancarios;
+    private String observacoes;
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(String documento) {
+        this.documento = documento;
+    }
+
+    public String getRazaoSocial() {
+        return razaoSocial;
+    }
+
+    public void setRazaoSocial(String razaoSocial) {
+        this.razaoSocial = razaoSocial;
+    }
+
+    public String getNomeFantasia() {
+        return nomeFantasia;
+    }
+
+    public void setNomeFantasia(String nomeFantasia) {
+        this.nomeFantasia = nomeFantasia;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getDadosBancarios() {
+        return dadosBancarios;
+    }
+
+    public void setDadosBancarios(String dadosBancarios) {
+        this.dadosBancarios = dadosBancarios;
+    }
+
+    public String getObservacoes() {
+        return observacoes;
+    }
+
+    public void setObservacoes(String observacoes) {
+        this.observacoes = observacoes;
+    }
+}
